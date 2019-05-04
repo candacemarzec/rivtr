df7f2a4913716c68826ecb22336efcacc17ca27b
callaway <kcallawayworks@gmail.com>
2019-05-04 17:01:00 -0600

    candace it works

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,6 +1,6 @@
 const Category = require('./models/category.model');
 
 module.exports =  function(req, res) {
-  res.send('Greetings from the test controller');
+  res.send('Greetings from the test controller!!! Candace you freaking ROCK! Nice work!');
 };
   
